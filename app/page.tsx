14fccc966988798d589d63c0c98ba3839c42d7f3
YounusHussain9 <younushussain0@gmail.com>
2024-11-21 16:54:57 +0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
 export default function Home() {
-  const hi = 3;
-  return (
-    <>
-      {" "}
-      Test
-      <div>]]]\\\
-
-
-
-
-
-        
-      </div>
-    </>
-  );
+  return <> Test</>;
 }
